62feaa6c907bee802fcd1aa4ba32077769c010e6
tamcony <tamcony@icloud.com>
2024-04-28 18:02:52 +0800

    up

--- src/components/custom/blog/body/card/BodyCardList.vue
+++ src/components/custom/blog/body/card/BodyCardList.vue
@@ -1,16 +1,60 @@
 <template>
   <div class="flex flex-col overflow-y-scroll scrollbar-hide gap-3">
-    <template v-for="item in data?.items || []">
+    <template v-for="item in items || []">
       <BodyCardListItem :item="item"></BodyCardListItem>
     </template>
   </div>
 </template>
 
-<script lang='ts' setup>
+<script
+  lang='ts'
+  setup
+>
 import { Item } from '@/models/blog/card'
 import { useCardList } from '@/apis/blog/useCard'
 const { run, data, loading } = useCardList()
-const items = ref<Item[]>()
+const items = ref<Item[]>([
+  {
+    id: 1,
+    title: "这是第一条数据",
+    description: "这是第一条数据的描述",
+    image: "https://example.com/image1.jpg",
+    date: "2024-04-13",
+    author: {
+      id: 1,
+      name: "作者1",
+      avatar: "https://example.com/avatar1.jpg"
+    },
+    tags: [
+      { id: 1, name: "标签1" },
+      { id: 2, name: "标签2" }
+    ],
+    stat: {
+      like: 10,
+      comment: 5
+    }
+  },
+  {
+    id: 2,
+    title: "这是第二条数据",
+    description: "这是第二条数据的描述",
+    image: "https://example.com/image2.jpg",
+    date: "2024-04-14",
+    author: {
+      id: 2,
+      name: "作者2",
+      avatar: "https://example.com/avatar2.jpg"
+    },
+    tags: [
+      { id: 3, name: "标签3" },
+      { id: 4, name: "标签4" }
+    ],
+    stat: {
+      like: 20,
+      comment: 8
+    }
+  }
+])
 const params = reactive({
   pageNum: 1,
   pageSize: 20,
--- src/components/custom/blog/body/card/BodyCardListItem.vue
+++ src/components/custom/blog/body/card/BodyCardListItem.vue
@@ -1,33 +1,58 @@
 <template>
-  <div class="w-130 shrink-0 border-rd-3 bg-red-400 p-4 flex flex-row gap-14">
-    <div class="w-30 h-30 bg-#fff rounded-md">
+  <div class="w-130 shrink-0 border-rd-3 bg-red-400 p-4 flex flex-row gap-8 justify-between">
+    <div class="w-30 h-30 bg-#fff rounded-md flex shrink-0">
       <img
         :src="item.image"
         :alt="item.title"
       >
     </div>
-    <div class="flex flex-col flex-1">
-      <h1>{{ item.title }}</h1>
-      <span>{{ item.description }}</span>
-      <div class="flex flex-row">
-        <a-avatar
-          :icon="item.author?.avatar"
-          :size="12"
-        ></a-avatar>
-        <span>{{ item.author?.name }}</span>
+    <div class="flex flex-col justify-between h-full w-full">
+      <div class="flex flex-row justify-between h-full">
+        <div class="flex flex-col flex-1 text-4 justify-between">
+          <div class="flex flex-col">
+            <h1 class="font-600">{{ item.title }}</h1>
+            <span>{{ item.description }}</span>
+          </div>
+          <div class="flex flex-row">
+            <a-avatar
+              :icon="item.author?.avatar"
+              :size="24"
+            ></a-avatar>
+            <span>{{ item.author?.name }}</span>
+          </div>
+
+        </div>
+        <div class="flex">
+          <span>time</span>
+        </div>
       </div>
-      <div class="flex flex-row">
-        <template v-for="tag in item.tags">
-          <a-tag>
-            {{ tag.name }}
-          </a-tag>
-        </template>
+      <div class="flex flex-row justify-between">
+        <div class="flex flex-row">
+          <template v-for="tag in item.tags">
+            <a-tag>
+              {{ tag.name }}
+            </a-tag>
+          </template>
+        </div>
+        <div class="flex flex-row justify-between">
+          <div class="flex flex-row text-4 justify-between items-center">
+            <i-icon-park-outline:like></i-icon-park-outline:like>
+            <span>{{ item.stat.like }}</span>
+            <i-pajamas:comment-dots></i-pajamas:comment-dots>
+            <span>{{ item.stat.comment }}</span>
+          </div>
+          <a-button>readMore</a-button>
+        </div>
       </div>
     </div>
+
   </div>
 </template>
 
-<script lang='ts' setup>
+<script
+  lang='ts'
+  setup
+>
 import { Item } from '@/models/blog/card'
 
 interface Props {
